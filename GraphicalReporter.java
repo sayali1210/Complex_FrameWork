5125ecb45b510ad0a05bce25175b301e0976e55e
sayali <sayalidhamale12@gmail.com>
2023-05-14 18:22:02 +0530

    1234_LoginFature:remove 2nd line

--- GraphicalReporter.java
+++ GraphicalReporter.java
@@ -12,7 +12,6 @@ public class GraphicalReporter {
 	  ExtentReports report=new ExtentReports("E:/PROGRAMS/complex_Design_Patttern_Framework/a.html",true);
 	  ExtentTest test=report.startTest("My Sample Report");
 	  test.log(LogStatus.PASS,"I have successfully open a browser");
-	  test.log(LogStatus.ERROR,"My title is not matched with expected "); 
 	  test.log(LogStatus.FAIL,"Logout is available on different location");
 	  test.log(LogStatus.WARNING,"No susch a element exception");
       report.endTest(test);
